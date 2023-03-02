1b198f209a1d2a59ccf9a2d808ebc120225e9ab8
thibaultcharrin <thibault.charrin@gmail.com>
2023-03-02 13:29:46 +0100

    etl.sh executable from desktop AND default IMPORT EXPORT folders

--- etl.sh
+++ etl.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 # VARIABLES
-WORKSPACE=$(pwd)
-INPUT="$1"
-OUTPUT="$2"
+INPUT="IMPORT"
+OUTPUT="EXPORT"
 
-if [[ "$1" == "" ]] || [[ "$2" == "" ]]; then echo "Usage: ./etl.sh SOURCE_FOLDER DESTINATION_FOLDER"; exit 1; fi
+# ARGS
+if [[ "$1" != "" ]]; then INPUT="$1"; fi
+if [[ "$2" != "" ]]; then OUTPUT="$2"; fi
+
+if [[ ! -d $INPUT ]]; then
+    echo "Please add source folder (default: IMPORT), or specify name when running script"
+    echo -e "\t./elt.sh [SOURCE_FOLDER]"
+    echo "You can also specify the destination folder (default: EXPORT)"
+    echo -e "\t./elt.sh [SOURCE_FOLDER] [DESTINATION_FOLDER]"
+    read -rp "Press enter to exit"
+    exit 0
+fi
 
 # FUNCTIONS
 function whitespace_to_underscore() {
     echo -e "\n*** Replacing whitespaces with underscores in ${INPUT} ***"
-    while [[ $(find -type f | grep " " | wc -l) != 0 ]]
+    while [[ $(find -type f | grep -c " ") != 0 ]]
     do    
         find -type d -name "* *" -print | awk '{print;gsub(" ","_",$0);print}' | xargs -r -n2 -d "\n" mv
         find -type f -name "* *" -print | awk '{print;gsub(" ","_",$0);print}' | xargs -r -n2 -d "\n" mv
     done
 }
 
-function ETL() {
+function etl() {
     echo -e "\n*** Extract Transform Load ***"
     ELEMENTS=$(find $INPUT -type f)
     echo -e "\tSOURCE: "
@@ -38,9 +48,10 @@ function ETL() {
     done
 }
 
+
 # STEPS
 whitespace_to_underscore
-ETL
+etl
 echo -e "\nYou're all set !"
-read -p "Press enter to exit"
+read -rp "Press enter to exit"
 exit 0
